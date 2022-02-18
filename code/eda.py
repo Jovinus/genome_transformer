c93b9ff222f71e3448c0522933e5dc8ca80eeb2d
KyungHyun Jovinus Lee <lkh256@gmail.com>
2022-02-18 15:56:09 +0900

    basic eda

--- code/eda.py
+++ code/eda.py
@@ -6,5 +6,13 @@ import matplotlib.pyplot as plt
 from IPython.display import display
 # %%
 df_orig = pd.read_csv("../data/train.csv", usecols=['id', 'genome_sequence', 'species'])
-df_orig.head()
+# %%
+df_orig = df_orig.assign(max_seq_len = lambda x: x['genome_sequence'].apply(len),
+                         unique_neuc = lambda x: x['genome_sequence'].apply(lambda y: sorted(list(set(y)))),
+                         unique_neuc_num = lambda x: x['unique_neuc'].apply(len))
+# %%
+df_orig['max_seq_len'].value_counts()
+
+# %%
+display(df_orig.head())
 # %%
